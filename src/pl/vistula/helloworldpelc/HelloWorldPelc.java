ef964057ea821e1f609311c1c15b9f28bd9c4626
Kpelc <kpelc1@stu.vistula.edu.pl>
2022-10-07 14:23:18 +0200

    Czwarty Commit

--- src/pl/vistula/helloworldpelc/HelloWorldPelc.java
+++ src/pl/vistula/helloworldpelc/HelloWorldPelc.java
@@ -4,8 +4,5 @@ public class HelloWorldPelc {
     public static void main(String[] args) {
         System.out.println("Hello Vistula Pelc");
         System.out.println("Taka akcja jest grana");
-        System.out.println("Takie sobie pisze bajery");
-
-
     }
 }
